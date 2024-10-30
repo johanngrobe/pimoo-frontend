50b9ffb858a1177cb7af862b74f38646d922e56a
Johann <johann.grobe@hs-rm.de>
2024-10-30 17:22:40 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+# Step 1: Pull the latest changes from the repository
+echo "Pulling the latest changes from the repository..."
+git pull
 
-# Step 1: Install dependencies
+# Step 2: Install dependencies
 echo "Installing dependencies..."
 npm install
 
-# Step 2: Build the Vue application
+# Step 3: Build the Vue application
 echo "Building the app..."
 npm run build
 
-# Step 3: Copy the build files to the deployment directory
+# Step 4: Copy the build files to the deployment directory
 TARGET_DIR="/var/www/pimoo-1rz.fab.hs-rm.de/html/"
 echo "Copying files to $TARGET_DIR"
 sudo cp -r dist/* "$TARGET_DIR"
